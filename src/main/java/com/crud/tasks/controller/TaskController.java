6ee39213928189486aef77549afe7dd97b6ca7b2
Robert Gajewski <robert.gajewski@nask.pl>
2021-05-13 16:58:35 +0200

    kodilla-tasks getTasksById

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,6 +1,11 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,16 +15,31 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/v1/task")
+@RequiredArgsConstructor
 public class TaskController {
+    private final DbService dbService;
+    private final TaskMapper taskMapper;
     private TaskDto taskDto;
     private final List<TaskDto> taskDtoList = new ArrayList<>();
 
-    @RequestMapping(method = RequestMethod.GET, value = "getTasks")
-    public List<TaskDto> getTasks() { return taskDtoList; }
-
     @GetMapping(value = "getTask")
     public TaskDto getTask(Long taskId) {
+
         return new TaskDto(1L, "test title", "test_content");
+    }
+
+    @GetMapping(value = "getTasks", params = "id")
+    public List<TaskDto> getTasks() {
+        List<Task> tasks = dbService.getAllTasks();
+        return taskMapper.mapToTaskDtoList(tasks);
+    }
+
+    @GetMapping(value = "getTasks?id={taskId}")
+    public List<TaskDto> getTaskById(@PathVariable Long taskId) throws TaskNotFoundException  {
+        if (!dbService.getTaskById(taskId).isEmpty())
+            return taskMapper.mapToTaskDtoList(dbService.getTaskById(taskId));
+        else
+            return (List<TaskDto>) new TaskNotFoundException();
     }
 
     @DeleteMapping(value = "deleteTask/{taskId}")
